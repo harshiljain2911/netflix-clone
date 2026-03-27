91d5713d6c40ffa72fb9e40598211d9d222eac8c
Harshil Jain <kotaharshil2906@gmail.com>
2026-03-28 00:21:17 +0530

    removed pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
